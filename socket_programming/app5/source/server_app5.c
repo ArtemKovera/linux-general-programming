c27f12fe3600ef221ff379e4f2b639056a059a9a
Artem Kovera <artemkovera@gmail.com>
2021-09-19 07:35:01 +0300

    server

--- socket_programming/app5/source/server_app5.c
+++ socket_programming/app5/source/server_app5.c
@@ -13,11 +13,51 @@
 #include<sys/socket.h>
 #include<netdb.h>
 #include"robust_io.h"
-
-void echo(int connfd);
+#include"server_routines.h"
 
 int main(int argc, char **argv)
 {
+    if(argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    int listenFD, connFD;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    char clientHostname[MAXLINE];
+    char clientPort[MAXLINE];
+    
+    listenFD = openListenFD(argv[1]);
+
+    while(1)
+    {
+        clientlen = sizeof(struct sockaddr_storage);
+        connFD = accept(listenFD, (struct sockaddr*)&clientaddr, &clientlen);
+        if(connFD == -1)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        if(getnameinfo((struct sockaddr *) &clientaddr, clientlen, clientHostname, MAXLINE, clientPort, MAXLINE, 0))
+        {
+           perror("getnameinfo");
+           exit(EXIT_FAILURE);
+        }
+        
+        printf("Connected to (%s, %s)\n", clientHostname, clientPort);
+        
+        echo(connFD);
+
+        if(close(connFD))
+        {
+            perror("close");
+            exit(EXIT_FAILURE);
+        }
+        
+    }
 
     return 0;
 }
